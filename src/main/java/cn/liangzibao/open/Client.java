d574f85dcb153e0fd2011de9dcd4f1af7cda4ceb
wangbai <wangbai@greencheng.com>
2017-02-27 13:04:46 +0800

    完善client注释

--- src/main/java/cn/liangzibao/open/Client.java
+++ src/main/java/cn/liangzibao/open/Client.java
@@ -34,10 +34,30 @@ final public class Client {
 
     private HashMap<String, String> params;
 
+    /**
+     * 量子保开放平台接口封装
+     *
+     * @param baseUrl 网关调用URL，不同环境该URL不同
+     * @param privateKey 开发者密钥对的私钥，用于对请求公共参数做签名，和对应用响应参数做解密
+     * @param lzbPublicKey 量子保开放平台对外公钥，用于对响应公共参数做验签，和对应用请求参数做加密
+     * @param appKey 量子保开放平台为开发者分配的唯一标示
+     */
     public Client(String baseUrl, String privateKey, String lzbPublicKey, String appKey) {
         this(baseUrl, privateKey, lzbPublicKey, appKey, "1.0.0", "JSON", "RSA", "UTF-8");
     }
 
+    /**
+     * 量子保开放平台接口封装
+     *
+     * @param baseUrl 网关调用URL，不同环境该URL不同
+     * @param privateKey 开发者密钥对的私钥，用于对请求公共参数做签名，和对应用响应参数做解密
+     * @param lzbPublicKey 量子保开放平台对外公钥，用于对响应公共参数做验签，和对应用请求参数做加密
+     * @param appKey 量子保开放平台为开发者分配的唯一标示
+     * @param version 接口版本，默认1.0.0
+     * @param format 应用调用报文格式，默认JSON
+     * @param signType 接口签名方式，默认RSA
+     * @param charset 报文字符编码，默认UTF-8
+     */
     public Client(String baseUrl,
                   String privateKey,
                   String lzbPublicKey,
@@ -67,6 +87,16 @@ final public class Client {
         this.params = new HashMap<>();
     }
 
+    /**
+     * 应用调用
+     *
+     * @param serviceName 应用接口标示
+     * @param bizParams 应用请求参数表
+     * @return 返回应用响应参数表，JSON对象
+     * @throws ProtocolException 网络传输层错误异常
+     * @throws SignVerificationError 响应报文签名验证失败
+     * @throws ResponseError 调用失败异常
+     */
     public JSONObject invoke(String serviceName, JSONObject bizParams)
             throws ProtocolException, SignVerificationError, ResponseError {
         String bizContent = PacketUtil.bizParamsEncrypt(this.lzbPublicKey, bizParams);
@@ -108,6 +138,13 @@ final public class Client {
         return PacketUtil.bizParamsDecrypt(this.privateKey, responseParams.get("biz_content"));
     }
 
+    /**
+     * 生成GET方式完整的调用的URL，仅用于标准HTML5页面接入
+     *
+     * @param serviceName 应用HTML5调用标示
+     * @param bizParams 应用请求参数表
+     * @return 完整调用URL，可以直接用于浏览器或者开发平台的Webview
+     */
     public String buildRequestUrl(String serviceName, JSONObject bizParams) {
         String bizContent = PacketUtil.bizParamsEncrypt(this.lzbPublicKey, bizParams);
         Long requestTime = new Date().getTime()/1000;
