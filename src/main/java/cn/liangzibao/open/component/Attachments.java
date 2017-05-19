b32391b2387f54e5dc6cb8a3f6f01e26dc1f3f8e
wangbai <wangbai@greencheng.com>
2017-05-19 18:11:08 +0800

    增加文件上传功能

---
+++ src/main/java/cn/liangzibao/open/component/Attachments.java
@@ -0,0 +1,40 @@
+package cn.liangzibao.open.component;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by wangbai on 2017/5/19.
+ */
+public class Attachments {
+
+    private Map<String, ArrayList<File>> attachmentList = null;
+
+    public Attachments() {
+        this.attachmentList = new HashMap<>();
+    }
+
+    public void addAttachment(String name, String filepath) {
+        File file = new File(filepath);
+        if (!file.exists()) {
+            return;
+        }
+
+        ArrayList fileList =  attachmentList.get(name);
+        if (fileList != null) {
+            fileList = new ArrayList<>();
+            this.attachmentList.put(name, fileList);
+        }
+        fileList.add(file);
+    }
+
+    public ArrayList<File> getAttachment(String name) {
+        return this.attachmentList.get(name);
+    }
+
+    public Map<String, ArrayList<File>> getAttachmentList() {
+        return this.attachmentList;
+    }
+}
--- src/main/java/cn/liangzibao/open/utils/ProtocolUtil.java
+++ src/main/java/cn/liangzibao/open/utils/ProtocolUtil.java
@@ -13,6 +13,7 @@
 
 package cn.liangzibao.open.utils;
 
+import cn.liangzibao.open.component.Attachments;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
@@ -20,6 +21,10 @@ import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.mime.HttpMultipartMode;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.apache.http.entity.mime.content.FileBody;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicNameValuePair;
@@ -27,6 +32,7 @@ import org.apache.http.util.EntityUtils;
 import org.json.simple.JSONObject;
 import org.json.simple.JSONValue;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -36,16 +42,19 @@ import java.util.Map;
 
 public class ProtocolUtil {
 
-    static final String USER_AGENT = "LZB/Openapi SDK/v1.1.0(Java)";
+    static final String USER_AGENT = "LZB/Openapi SDK/v1.2.0(Java)";
     static final String CONTENT_TYPE = "application/x-www-form-urlencoded;charset=utf-8";
 
-    static public Map<String, String> invoke(String Url, Map<String, String> params) throws IOException {
+    static public Map<String, String> invoke(String Url, Map<String, String> params, Attachments attachmentList) throws IOException {
         CloseableHttpClient httpclient = HttpClients.createDefault();
         try {
             final HttpPost requestMethod = new HttpPost(Url);
+            
             requestMethod.setHeader("User-Agent", USER_AGENT);
-            requestMethod.setHeader("Content-Type", CONTENT_TYPE);
-            requestMethod.setEntity(new UrlEncodedFormEntity(ProtocolUtil.buildRequest(params)));
+            if (attachmentList == null) {
+                requestMethod.setHeader("Content-Type", CONTENT_TYPE);
+            }
+            requestMethod.setEntity(ProtocolUtil.buildRequest(params, attachmentList));
 
             ResponseHandler<String> responseHandler = new ResponseHandler<String>() {
                 public String handleResponse(final HttpResponse response) throws IOException {
@@ -71,7 +80,11 @@ public class ProtocolUtil {
 
     static public String buildRequestUrl(String Url, Map<String, String> params) {
         try {
-            String urlParam = EntityUtils.toString(new UrlEncodedFormEntity(ProtocolUtil.buildRequest(params)));
+            List<NameValuePair> postParams = new ArrayList<NameValuePair>();
+            for (String key : params.keySet()) {
+                postParams.add(new BasicNameValuePair(key, params.get(key)));
+            }
+            String urlParam = EntityUtils.toString(new UrlEncodedFormEntity(postParams));
 
             return Url + "?" + urlParam;
         } catch(Exception e) {
@@ -80,14 +93,40 @@ public class ProtocolUtil {
         return null;
     }
 
-    static private List<NameValuePair> buildRequest(Map<String, String> params) {
-        List<NameValuePair> postParams = new ArrayList<NameValuePair>();
+    static private HttpEntity buildRequest(Map<String, String> params, Attachments attachmentList) {
+        if (attachmentList == null) {
+            List<NameValuePair> postParams = new ArrayList<NameValuePair>();
 
-        for (String key : params.keySet()) {
-            postParams.add(new BasicNameValuePair(key, params.get(key)));
+            for (String key : params.keySet()) {
+                postParams.add(new BasicNameValuePair(key, params.get(key)));
+            }
+
+            try {
+                HttpEntity entity = new UrlEncodedFormEntity(postParams);
+                return entity;
+            } catch(Exception e) {
+                return null;
+            }
+        } else {
+            MultipartEntityBuilder mpBuilder = MultipartEntityBuilder.create();
+            mpBuilder.setMode(HttpMultipartMode.BROWSER_COMPATIBLE);
+
+            Map<String, ArrayList<File>> allAttachments = attachmentList.getAttachmentList();
+            for (String key : allAttachments.keySet()) {
+                ArrayList<File> fileList = allAttachments.get(key);
+                if (fileList.size() == 1) {
+                    mpBuilder.addPart(key, new FileBody(fileList.get(0), ContentType.DEFAULT_BINARY));
+                } else {
+                    int i = 0;
+                    for (File file : fileList) {
+                        mpBuilder.addPart(key+"["+i+"]", new FileBody(file, ContentType.DEFAULT_BINARY));
+                        ++i;
+                    }
+                }
+            }
+
+            return mpBuilder.build();
         }
-
-        return postParams;
     }
 
     static private Map<String, String> parseResponse(String responseBody) {
